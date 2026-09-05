c1cb5629793c7c9fca63cc1800b7b7b040e7674e
nunez25 <jmmorenonunez2024@hccci.edu.ph>
2026-09-05 20:04:01 +0800

    recipe

--- src/components/CategoryFilter.jsx
+++ src/components/CategoryFilter.jsx
@@ -4,18 +4,7 @@ export default function CategoryFilter({ activeFilter, onFilterChange }) {
   return (
     <div className="flex flex-wrap gap-2 justify-center">
 
-      {/* {<button>
-          onClick={() => onFilterChange(f)}
-          className=[activeFilter === f ?]
-          {f}
-        </button>
-      } */}
-      {/* TODO: render one button per FILTERS entry.
-          - Each button calls onFilterChange(f) on click.
-          - The active button should look different from the others
-            (e.g., "btn-primary" for active, "btn-outline" otherwise).
-          - Don't forget the `key` prop! */}
-          {FILTERS.map((f) => (
+      {FILTERS.map((f) => (
             <button
               key={f}
               onClick={() => onFilterChange(f)}
@@ -26,7 +15,11 @@ export default function CategoryFilter({ activeFilter, onFilterChange }) {
               {f}
             </button>
           ))}
-
+      {/* TODO: render one button per FILTERS entry.
+          - Each button calls onFilterChange(f) on click.
+          - The active button should look different from the others
+            (e.g., "btn-primary" for active, "btn-outline" otherwise).
+          - Don't forget the `key` prop! */}
     </div>
   );
 }
